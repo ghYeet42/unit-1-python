8a4335fd8290d8e1c46c07e2ac78e65455814d5d
ghyeet42 <carterfscarlett@gmail.com>
2023-09-19 09:52:17 -0400

    pexercises (py-exercises)

---
+++ benchmark.py
@@ -0,0 +1,52 @@
+# exe. 1: Display your name in the console.
+
+x = ("Hi, my name is carter! ")
+
+y = ("I'm 17 years old. ")
+
+print(x + y)
+
+# # exe. 2: Create a variable called favoriteShow and set it equal to the name of your favorite show.
+
+favoriteShow = ("My favorite show is Beyblade!")
+
+print(x + y + favoriteShow)
+
+# # # exe. 3: Create two variable called height and age, then display them in the console, the product of them.
+
+height = (5.58)
+
+age = (21)
+
+print(height * age)
+
+# # # # exe. 4: Display "im going to break" in the console.
+
+print("im going to break")
+
+# # # # # exe. 5: Create a loist of strings called "fav_food", put 3 of your favorite food items inside of the list
+
+fav_food = ["kiwi", "avacado", "yellow apple"]
+
+# # # # # # exe. 6: Create a conditional statement that checkes if your age is grerater than 21, if true then display "you can rent a car" otherwise display "your beat"
+
+if (age >= 21):
+    print("you can rent a car")
+else:
+    print("your beat")
+
+# # # # # # # exe. 7: Ask the user for their name, then display in the console, "Hello, their_name"
+
+print("Enter your name:")
+their_name = input()
+print("Hello, " + their_name + "!")
+
+# # # # # # # # exe. 8: Create a looop that will display each item from fav_food on a seperate line in the console
+
+for food in fav_food:
+    print(food)
+
+# # # # # # # # # exe. 9: Create a while loop that will never run
+
+while 20 < 6:
+    print("Lol, ok..")
