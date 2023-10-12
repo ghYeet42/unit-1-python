57d59c319a3bdf45466e7eec0792a499261ff460
ghyeet42 <carterfscarlett@gmail.com>
2023-10-12 10:53:36 -0400

    bruhshhs

--- dub_dates.py
+++ dub_dates.py
@@ -16,16 +16,31 @@ print(theTimeRn.strftime("%T"))
 
 #Exercise 3: Using the strptime function, convert two strings into dates. Then find the difference in days between the two.
 
-typeYourDate = input("Type a date in DD/MM/YY order: ")
+from datetime import datetime
 
-typeYourDate2 = input("Type another date in DD/MM/YY order: ")
+string = "21 February, 2018"
 
-print(typeYourDate.strftime("%j"))
+string2 = "25 February, 2018"
+
+object = datetime.strptime(string, "%d %B, %Y")
+
+object2 = datetime.strptime(string2, "%d %B, %Y")
+
+print(object2 - object)
+
+#exp 3. imported datetime module and made 2 vars (string and string2) that hold dates, made 2 more vars (object and object2) that use datetime and strptime function to turn the strings into time formats for both object vars then printed object2 - object.
 
 
+#Excercise 4: Write a program that asks the user for their birthdate and calculates their current age using the datetime module.
 
-"""
-Excercise 4:
-Write a program that asks the user for their birthdate and calculates their current 
-age using the datetime module.
-"""
+import datetime
+
+from datetime import datetime
+
+typeYourDate = input("Type your birthday in 'Day Month, Year' order (ex: 16 June, 2020): ")
+
+typeYourDatee = datetime.strptime(typeYourDate, "%d %B, %Y")
+
+print("You are" + typeYourDatee("%Y") - theTimeRn("%Y") + "years old.") 
+
+#exp 4. imported datetime module(s) and made a var input typeYourDate and then another var, typeYourDatee, to convert the input into proper format and then subtract it from the var made previously, theTimeRn, to get the user's age
